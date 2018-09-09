97c5cb1dec37a062525b39ddf993f91b67dee2f8
Kenta11 <kk.068.kenta@gmail.com>
2018-09-09 09:13:08 +0900

    add function open and close

--- src/detect_color.cpp
+++ src/detect_color.cpp
@@ -23,28 +23,49 @@ bool DetectColor::detectRed(cv::Mat &image){
     cv::Mat mask1, mask2, mask;
     cv::inRange(hsv, lower1, upper1, mask1);
     cv::inRange(hsv, lower2, upper2, mask2);
-    cv::bitwise_and(mask1, mask2, mask);
-
-    // cv::imshow("hoge", mask2);
-    // cv::waitKey(1);
 
     // mask
-    cv::Mat masked;
-    image.copyTo(masked, mask);
+    cv::Mat masked1, masked2;
+    image.copyTo(masked1, mask1);
+    image.copyTo(masked2, mask2);
 
     // remove noise from a gray scale image
-    cv::Mat channels[3], opened, closed;
-    cv::split(masked, channels);
-    cv::morphologyEx(channels[2], closed, cv::MORPH_CLOSE, cv::Mat::ones(7, 7, CV_8U));
-    cv::morphologyEx(closed, opened, cv::MORPH_OPEN, cv::Mat::ones(5, 5, CV_8U));
+    cv::Mat channels[3], opened1, closed1;
+    // masked1
+    cv::split(masked1, channels);
+    cv::morphologyEx(channels[2], opened1, cv::MORPH_OPEN, cv::Mat::ones(3, 3, CV_8U));
+    cv::imwrite("opened1_0.jpg", opened1);
+    cv::morphologyEx(opened1, closed1, cv::MORPH_CLOSE, cv::Mat::ones(3, 3, CV_8U));
+    cv::imwrite("closed1_0.jpg", closed1);
+    for (int i = 0; i < 3; i++) {
+        cv::morphologyEx(closed1, opened1, cv::MORPH_OPEN, cv::Mat::ones(i * 2 + 5, i * 2 + 5, CV_8U));
+        cv::imwrite(std::string("opened1_") + std::to_string(i + 1) + std::string(".jpg"), closed1);
+        cv::morphologyEx(opened1, closed1, cv::MORPH_CLOSE, cv::Mat::ones(i * 2 + 5, i * 2 + 5, CV_8U));
+        cv::imwrite(std::string("closed1_") + std::to_string(i + 1) + std::string(".jpg"), closed1);
+    }
+
+    // masked1
+    cv::Mat opened2, closed2;
+    cv::split(masked2, channels);
+    cv::morphologyEx(channels[2], opened2, cv::MORPH_OPEN, cv::Mat::ones(3, 3, CV_8U));
+    cv::imwrite("opened2_0.jpg", opened2);
+    cv::morphologyEx(opened2, closed2, cv::MORPH_CLOSE, cv::Mat::ones(3, 3, CV_8U));
+    cv::imwrite("closed2_0.jpg", closed2);
+    for (int i = 0; i < 3; i++) {
+        cv::morphologyEx(closed2, opened2, cv::MORPH_OPEN, cv::Mat::ones(i * 2 + 5, i * 2 + 5, CV_8U));
+        cv::imwrite(std::string("opened2_") + std::to_string(i + 1) + std::string(".jpg"), closed2);
+        cv::morphologyEx(opened2, closed2, cv::MORPH_CLOSE, cv::Mat::ones(i * 2 + 5, i * 2 + 5, CV_8U));
+        cv::imwrite(std::string("closed2_") + std::to_string(i + 1) + std::string(".jpg"), closed2);
+    }
+
+    // concatenate
+    cv::Mat concat;
+    cv::bitwise_and(closed1, closed2, concat);
+    cv::imwrite("concat.jpg", concat);
 
     cv::imwrite("hsv.jpg", hsv);
     cv::imwrite("mask1.jpg", mask1);
     cv::imwrite("mask2.jpg", mask2);
-    cv::imwrite("mask.jpg", mask);
-    cv::imwrite("masked.jpg", masked);
-    cv::imwrite("opened.jpg", opened);
-    cv::imwrite("closed.jpg", closed);
 
     return true;
 }
